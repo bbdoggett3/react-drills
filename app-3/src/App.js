5c8e2e3313bde05b42787a34335ca6306f366d25
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-13 14:37:46 -0600

    Finished app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      favMovies: ["Back to the Future", "Tron: Legacy", "Iron Man", "Ford v. Ferrari", ],
+      filterString: ""
+    }
+  }
+
+  eventHandler(filter) {
+    this.setState({filterString: filter})
+  }
+
+  render() {
+    let displayMovie = this.state.favMovies.filter((element, index) => {
+      return element.includes(this.state.filterString);
+    })
+
+    .map((element, index) => {
+      return <h2 key = {index} >{element}</h2>
+    });
+
+    return (
+      <div className="App">
+        <input onChange= {event => this.eventHandler(event.target.value)} type="text"></input>
+        {displayMovie};
+      </div>
+    );
+  }
 }
 
 export default App;
